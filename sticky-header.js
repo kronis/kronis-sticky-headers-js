b8301bae5b6a4df3fe1c2aec9d0332bb81485c60
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2015-04-20 08:23:20 +0200

    Updated sticky headers to re-initialize on resizing

--- sticky-header.js
+++ sticky-header.js
@@ -2,35 +2,61 @@ $.fn.sticky = function() {
     'use strict';
     return this.each(function() {
         var $this = $(this);
+        var doResizing;
+
+        function resetHeaderInfo() {
+            $this.find('tbody tr:first td').css('width', '');
+            $this.find('th').css('min-width', '');
+            $this.find('thead.sticky').removeClass('sticky');
+            $this.find('thead.sticky-clone-placeholder').remove();
+        }
 
         function init() {
-            $this.find('th').each(function(index) {
-                $(this).css('width', $this.find('th').eq(index).outerWidth() + 'px');
+            resetHeaderInfo();
+            $this.find('tbody tr:first td').each(function() {
+                $(this).css('width', $(this).css('width', '').outerWidth() + 'px');
+            });
+            $this.find('th').each(function() {
+                $(this).css('min-width', $(this).outerWidth() + 'px');
             });
 
-            $this.find('tbody tr:first td').each(function() {
-                $(this).css('width', $(this).outerWidth() + 'px');
-            });
         }
 
         function scrollFixed() {
             var offset = $(window).scrollTop();
             var tableOffsetTop = $this.offset().top;
             if (offset > tableOffsetTop) {
+                // Create a cloned placeholder
                 if ($this.find('thead.sticky').size() === 0) {
                     $this.find('thead').addClass('sticky');
                     var clonedHead = $this.find('thead').clone();
-                    clonedHead.removeAttr('data-reactid');
                     clonedHead.find('[data-reactid]').removeAttr('data-reactid');
                     clonedHead.addClass('sticky-clone-placeholder').removeClass('sticky');
                     $this.prepend(clonedHead);
                 }
+
+                // Fulfill scrolling left and right
+                var scrollLeft = $(window).scrollLeft();
+                var offsetLeft = $this.find('.sticky-clone-placeholder').offset().left;
+                $this.find('thead.sticky').css('left', -1 * scrollLeft + offsetLeft);
             } else {
                 $this.find('thead.sticky').removeClass('sticky');
                 $this.find('thead.sticky-clone-placeholder').remove();
             }
         }
+
+        function resize() {
+            init();
+            setTimeout(scrollFixed, 100);
+        }
+
+        function waitForResize() {
+            clearTimeout(doResizing);
+            doResizing = setTimeout(resize, 100);
+        }
         $(window).scroll(scrollFixed);
+        $(window).resize(waitForResize);
         init();
+        scrollFixed();
     });
 };
